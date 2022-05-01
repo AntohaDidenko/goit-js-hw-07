f766bd93a375859746783d23ec2ffe41ab8073ca
Anton <didenkofficial@gmail.com>
2022-05-01 22:47:44 +0300

    Готово

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,44 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const makeGallery = galleryItems.map(image =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img
+            class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+            />
+        </a>
+    </li>`).join('');
+
+gallery.innerHTML = makeGallery;
+
+const instance = basicLightbox.create(
+        `<img class="modal-img">`,
+        {
+            onShow: () => window.addEventListener('keydown', onEscButtonPress),
+            onClose: () => window.addEventListener('keydown', onEscButtonPress),
+        }
+    );
+
+gallery.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+    
+    instance.element().querySelector('.modal-img').src = e.target.dataset.source;
+    instance.show();
+});
+
+function onEscButtonPress(e) {
+    if (e.key === 'Escape') {
+        instance.close();
+        window.removeEventListener('keydown', onEscButtonPress);
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const makeGallery = galleryItems.map(image =>
+    `<a class="gallery__item" href="${image.original}">
+        <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+    </a>`).join('');
+
+gallery.innerHTML = makeGallery;
+
+const instance = new SimpleLightbox('.gallery a', {
+    fadeSpeed: 250,
+    overlayOpacity: 0.7,
+    captionsData: 'alt',
+})
